090987f21368ed7a86c9d0b8bc3a00bc35556da7
Oliver Sauder <os@esite.ch>
2017-05-30 17:27:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+cookiecutter-django-json-api
+============================
+
+Default cookiecutter template to start a new django json api project
+
+Requirements
+------------
+Install `cookiecutter` command line: `pip install cookiecutter`    
+
+Usage
+-----
+Generate a new Cookiecutter template layout: `cookiecutter gh:sliverc/cookiecutter-django-json-api`    
---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,13 @@
+"""Setuptools package definition."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='{{cookiecutter.project_name}}',
+    version='0.0.0',
+    author='Adfinis SyGroup AG',
+    author_email='https://adfinis-sygroup.ch/',
+    description='{{cookiecutter.description}}',
+    url='https://adfinis-sygroup.ch/',
+    packages=find_packages(),
+)
