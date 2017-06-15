04c2e3ffb694c2794ae28ab0a00bb0359d686ad3
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-06-15 16:14:16 +0200

    Adjusted invalid django_app replacement string

--- {{cookiecutter.project_name}}/backend/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/backend/{{cookiecutter.project_name}}/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
-    url(r'^api/v1/', include('{{cookiecutter.project_name}}.{{django_app}}.urls')),
+    url(r'^api/v1/', include('{{cookiecutter.project_name}}.{{cookiecutter.django_app}}.urls')),
 ]
