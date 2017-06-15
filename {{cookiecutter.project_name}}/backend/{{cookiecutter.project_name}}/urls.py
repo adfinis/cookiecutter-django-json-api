397dd53aaa02376913cdcb4eb2107eba4c7b50ba
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-06-15 14:23:02 +0200

    Integrated jwt

--- {{cookiecutter.project_name}}/backend/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/backend/{{cookiecutter.project_name}}/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api/v1/', include('{{cookiecutter.project_name}}.{{django_app}}.urls')),
 ]
