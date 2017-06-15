6595ba46d54f91d492e3b5bbefd26440a0c8e67e
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-06-15 13:45:27 +0200

    Configure postgres as default database

--- {{cookiecutter.project_name}}/backend/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/backend/{{cookiecutter.project_name}}/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = '{{cookiecutter.project_name}}.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     '{{cookiecutter.project_name}}',
+        'USER':     '{{cookiecutter.project_name}}',
+        'PASSWORD': '{{cookiecutter.project_name}}',
+        'HOST':     'localhost'
     }
 }
 
