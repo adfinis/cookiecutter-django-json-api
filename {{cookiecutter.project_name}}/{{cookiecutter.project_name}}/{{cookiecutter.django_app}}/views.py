84f0b8e834fa6996453333b027322e1bf37d4ee3
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-07-31 14:08:06 +0200

    Introduce pytest factory boy and randomly
    Using pytest factory boy makes it easier to have a fix data set
    to test with.

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.django_app}}/views.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.django_app}}/views.py
@@ -5,7 +5,9 @@ from . import models, serializers
 
 class UserViewSet(views.ModelViewSet):
     serializer_class = serializers.UserSerializer
+    queryset = models.User.objects
 
     def get_queryset(self):
+        queryset = super().get_queryset()
         user = self.request.user
-        return models.User.objects.filter(id=user.id)
+        return queryset.filter(id=user.id)
