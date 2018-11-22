08c43dd731935ef555a2260e3ef13fc7294a5ab5
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-11-22 16:06:12 +0100

    Set a database option default compatible with all backends
    sslmode is only available on postgres but DATABASE_ENGINE can be changed
    to anything else so the default should be compatible with those.
    
    As sslmode is default to prefer on postgres anyway this won't make a
    difference.

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
         ),
         "HOST": env.str("DATABASE_HOST", default="localhost"),
         "PORT": env.str("DATABASE_PORT", default=""),
-        "OPTIONS": env.dict("DATABASE_OPTIONS", default={"sslmode": "prefer"}),
+        "OPTIONS": env.dict("DATABASE_OPTIONS", default={}),
     }
 }
 
